31d78920f8653ca682ed4fc0fb61268f7863c1db
xoxolische <nikitos_ua@ukr.net>
2018-09-19 17:10:25 +0300

    MAT-5176 message improve

--- src/main/java/net/anotheria/tcl/guitestbackend/notification/NotificationSlackSender.java
+++ src/main/java/net/anotheria/tcl/guitestbackend/notification/NotificationSlackSender.java
@@ -5,11 +5,17 @@ import com.github.seratch.jslack.api.methods.MethodsClient;
 import com.github.seratch.jslack.api.methods.SlackApiException;
 import com.github.seratch.jslack.api.methods.request.chat.ChatPostMessageRequest;
 import com.github.seratch.jslack.api.methods.response.chat.ChatPostMessageResponse;
+import com.github.seratch.jslack.api.model.Attachment;
 import net.anotheria.tcl.guitestbackend.TestCases;
 import org.configureme.ConfigurationManager;
 import org.configureme.annotations.ConfigureMe;
 
+import javax.print.DocFlavor;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 
 @ConfigureMe(allfields = true, name = "notification-slack-configuration")
 public class NotificationSlackSender {
@@ -20,8 +26,8 @@ public class NotificationSlackSender {
     private String botApiToken;
     private String slackChannel;
 
-    public static void main(String... a) {
-        new NotificationSlackSender().sendMessage(new CaseResultMessage("764364638463473jhb433", TestCases.REGISTRATION_REGISTRATION, false));
+    public static void main(String... a) throws MalformedURLException {
+        new NotificationSlackSender().sendMessage(new CaseResultMessage("catchall-1536278401658@adoptix.de", TestCases.REGISTRATION_REGISTRATION, false));
     }
 
     private NotificationSlackSender() {
@@ -34,12 +40,15 @@ public class NotificationSlackSender {
             String text = "";
             text += resultMessage.getTestCase().getCaseName() + " " + resultMessage.getStatus() + "\r\n";
             text += "User : " + resultMessage.getUserName() + "\r\n";
+            text += "Creation Timestamp : " + resultMessage.getUserName().split("@")[0].split("-")[1];
 
+            List<Attachment> attachments = new LinkedList<>();
+            attachments.add(Attachment.builder().title(text).color(resultMessage.isSuccess() ? "good" : "danger").build());
             ChatPostMessageRequest.ChatPostMessageRequestBuilder builder = ChatPostMessageRequest.builder()
                     .token(botApiToken)
                     .asUser(false)
-                    .text(text)
-                    .attachments(null);
+                    //.text(text)
+                    .attachments(attachments);
 
             builder.channel(slackChannel);
             ChatPostMessageResponse postMessageResponse = methodsClient.chatPostMessage(builder.build());
